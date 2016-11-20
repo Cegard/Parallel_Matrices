86e52eb7607e9e1335947b86300d77ced61a4ee2
Eduardo Galeano <cegard689@gmail.com>
2016-11-20 11:52:48 -0500

    sequential matrix multiplication created, vector add with cuda added.

---
+++ cudaMatrices.cu
@@ -0,0 +1,86 @@
+#include <cuda.h>
+#include <cuda_runtime.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string>
+
+#define MATRIX_DIM 10
+
+/*
+__global__ getCell(double *row, double *col, length){
+    
+    int i = threadIdx.x;
+    
+    
+}
+*/
+
+
+__host__ double* createArrayWithRandoms(){
+    double *matrix = (double *) malloc(MATRIX_DIM * sizeof(double));
+    
+    for (int i = 0; i < MATRIX_DIM; i++)
+        *(matrix + i) = (10.0*rand()/(RAND_MAX+1.0));
+    
+    return matrix;
+}
+
+
+__global__ void addArrays(double *a, double *b, double *c, int length){
+    int i = blockDim.x * blockIdx.x + threadIdx.x;
+    
+    if (i < length)
+        *(c + i) = *(a + i) + *(b + i);
+}
+
+
+__host__ void checkError(cudaError_t error, const char *point){
+    
+    if (error != cudaSuccess){
+        printf("there was an error at %s, error code: %d", point, error);
+        exit(EXIT_FAILURE);
+    }
+}
+
+
+int main(){
+    
+    cudaError_t error = cudaSuccess;
+    size_t size = MATRIX_DIM * sizeof(double);
+    double *h_a = createArrayWithRandoms();
+    double *h_b = createArrayWithRandoms();
+    double *h_c = (double *) malloc(MATRIX_DIM * sizeof(double));
+    double *d_a , *d_b, *d_c;
+    
+    error = cudaMalloc(&d_a, size);
+    checkError(error, "allocating device memory for A");
+    error = cudaMalloc(&d_b, size);
+    checkError(error, "allocating device memory for B");
+    error = cudaMalloc(&d_c, size);
+    checkError(error, "allocating device memory for C");
+    
+    cudaMemcpy(d_a, h_a, size, cudaMemcpyHostToDevice);
+    checkError(error, "copy A from host to device");
+    cudaMemcpy(d_b, h_b, size, cudaMemcpyHostToDevice);
+    checkError(error, "copy B from host to device");
+    cudaMemcpy(d_c, h_c, size, cudaMemcpyHostToDevice);
+    checkError(error, "copy C from host to device");
+    
+    addArrays<<< 1, MATRIX_DIM >>>(d_a, d_b, d_c, MATRIX_DIM);
+    
+    cudaMemcpy(h_c, d_c, size, cudaMemcpyDeviceToHost);
+    checkError(error, "copy C from device to host");
+    
+    for (int i = 0; i < MATRIX_DIM; i++)
+        printf("%.2f + %.2f = %.2f \n", *(h_a + i), *(h_b + i), *(h_c + i));
+    
+    free(h_a);
+    free(h_b);
+    free(h_c);
+    
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
+    
+    return 0;
+}
---
+++ sequential.cpp
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUM_THREADS 16
+#define MATRIX_DIM 8
+
+using namespace std; 
+
+
+double** createMatrixWithZeroes(){
+    double **matrix = (double**) malloc(MATRIX_DIM * sizeof(double*));
+    
+    for (int i = 0; i < MATRIX_DIM; i++){
+        *(matrix + i) = (double*) malloc(MATRIX_DIM * sizeof(double));
+        
+        for (int j = 0; j < MATRIX_DIM; j++)
+            *(*(matrix + i) + j) = 0.0;
+    }
+    
+    return matrix;
+}
+
+
+int printMatrix(double **matrix){
+    
+    for (int i = 0; i < MATRIX_DIM; i++){
+        
+        for (int j = 0; j < MATRIX_DIM; j++)
+            printf("%.2f ", *(*(matrix + i) + j));
+		
+        printf("\n");
+    }
+    printf("\n");
+}
+
+
+void freeMatrix(double ***matrix){
+    
+    for (int i = 0; i < MATRIX_DIM; i++)
+        free(*(*matrix + i));
+    
+    free(*matrix);
+}
+
+
+double ** createRandomMatrix(){
+    double **matrix = (double**) malloc(MATRIX_DIM * sizeof(double*));
+    
+    for (int i = 0; i < MATRIX_DIM; i++){
+        *(matrix + i) = (double*) malloc(MATRIX_DIM * sizeof(double));
+        
+        for (int j = 0; j < MATRIX_DIM; j++)
+            *(*(matrix + i) + j) = (10.0*rand()/(RAND_MAX+1.0));
+    }
+    
+    return matrix;
+}
+
+
+double** multiplyMatrix(double **matrixA, double **matrixB){
+    double **matrixC = createMatrixWithZeroes();
+    
+    for (int i = 0; i < MATRIX_DIM*MATRIX_DIM; i++){
+		double cell = 0.0;
+		int row = i/MATRIX_DIM;
+		int col = i%MATRIX_DIM;
+		
+		for (int j = 0; j < MATRIX_DIM; j++)
+			cell += *(*(matrixA + row) + j) * *(*(matrixB + j) + col);
+		
+		*(*(matrixC + row) + col) = cell;
+    }
+    
+    return matrixC;
+}
+
+
+int main(){
+    double **matrixA = createRandomMatrix();
+    double **matrixB = createRandomMatrix();
+	double **matrixC = multiplyMatrix(matrixA, matrixB);
+	
+	printf("\n");
+    printf("matrix a\n");
+    printMatrix(matrixA);
+    printf("matrix b\n");
+    printMatrix(matrixB);
+	printf("matrix c\n");
+	printMatrix(matrixC);
+	
+    freeMatrix(&matrixA);
+    freeMatrix(&matrixB);
+	freeMatrix(&matrixC);
+    
+	return 0;
+}
