8defd653d29cd72de5a635c1a09bef614780ab63
Eduardo Galeano <cegard689@gmail.com>
2016-11-20 21:41:14 -0500

    matrices multiplication ok using thrust library

--- cudaMatrices.cu
+++ cudaMatrices.cu
@@ -3,6 +3,7 @@
 #include <thrust/copy.h>
 #include <thrust/transform.h>
 #include <thrust/functional.h>
+#include <thrust/reduce.h>
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -17,6 +18,7 @@ int main(){
     thrust::device_vector<double> d_b = h_b;
     thrust::device_vector<double> d_c = h_c;
     
+    /* Vectors  initiallisation*/
     for (int i = 0; i < MATRIX_DIM; i++){
         
         for (int j = 0; j < MATRIX_DIM; j++){
@@ -27,10 +29,26 @@ int main(){
         }
     }
     
-    for(int i = 0; i < MATRIX_DIM; i++){
+    for (int i = 0; i < MATRIX_DIM; i ++){
+        
+        for (int j = 0; j < MATRIX_DIM; j++){
+            thrust::device_vector<double> row_times_col(MATRIX_DIM);
+            thrust::transform(d_a.begin() + (i * MATRIX_DIM),
+                              d_a.begin() + (i * MATRIX_DIM) + MATRIX_DIM,
+                              d_b.begin() + (j * MATRIX_DIM), row_times_col.begin(),
+                              thrust::multiplies<double>());
+            d_c[i * MATRIX_DIM + j] = thrust::reduce(row_times_col.begin(),
+                                                     row_times_col.end(), (double) 0.0,
+                                                     thrust::plus<double>());
+        }
+    }
+    
+    thrust::copy(d_c.begin(), d_c.end(), h_c.begin());
+    
+    for (int i = 0; i < MATRIX_DIM; i++){
         
         for(int j = 0; j < MATRIX_DIM; j++)
-            printf("%.2f ", h_b[i * MATRIX_DIM + j]);
+            printf("%.2f ", h_c[i * MATRIX_DIM + j]);
         
         printf("\n");
     }
