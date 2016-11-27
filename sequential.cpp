094c6b7621fd697b9a158fe6cf886443e90893d1
Eduardo Galeano <cegard689@gmail.com>
2016-11-27 12:14:09 -0500

    sequential implementation made based on omp implementation

--- sequential.cpp
+++ sequential.cpp
@@ -2,20 +2,27 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
+#define PAD 2*sizeof(double)
+
 using namespace std; 
 
 
-double** createMatrixWithZeroes(int dim){
-    double **matrix = (double**) malloc(dim * sizeof(double*));
+double** createMatrix(int dim, bool zeroes){
+    double **matrix = (double**) malloc(dim * sizeof(double*) * PAD);
     
     for (int i = 0; i < dim; i++){
-        *(matrix + i) = (double*) malloc(dim * sizeof(double));
+        *(matrix + i) = (double*) malloc(dim * sizeof(double) + PAD);
         
         for (int j = 0; j < dim; j++)
-            *(*(matrix + i) + j) = 0.0;
+            *(*(matrix + i) + j) = (zeroes)? 0.0 : (10.0*rand()/(RAND_MAX+1.0));
     }
     
     return matrix;
+}
+
+
+double** createMatrixWithZeroes(int dim){
+    return createMatrix(dim, 1);
 }
 
 
@@ -32,6 +39,11 @@ int printMatrix(double **matrix, int dim){
 }
 
 
+double ** createRandomMatrix(int dim){
+    return createMatrix(dim, 0);
+}
+
+
 void freeMatrix(double ***matrix, int dim){
     
     for (int i = 0; i < dim; i++)
@@ -41,70 +53,63 @@ void freeMatrix(double ***matrix, int dim){
 }
 
 
-double ** createRandomMatrix(int dim){
-    double **matrix = (double**) malloc(dim * sizeof(double*));
-    
-    for (int i = 0; i < dim; i++){
-        *(matrix + i) = (double*) malloc(dim * sizeof(double));
-        
-        for (int j = 0; j < dim; j++)
-            *(*(matrix + i) + j) = (10.0*rand()/(RAND_MAX+1.0));
-    }
-    
-    return matrix;
-}
-
-
-double** multiplyMatrix(double **matrixA, double **matrixB, double **matrixC, int dim){
+double** multiplyMatrix(double **matrixA, double **matrixB, double **matrixC, int dim, int threads){
     double **answer = matrixC;
     
-    for (int i = 0; i < dim*dim; i++){
+	for (int i = 0; i < dim; i++){
+	    
+	    for (int j = 0; j < dim; j++){
 		double cell = 0.0;
-		int row = i/dim;
-		int col = i%dim;
 		
-		for (int j = 0; j < dim; j++)
-			cell += *(*(matrixA + row) + j) * *(*(matrixB + j) + col);
+		for (int n = 0; n < dim; n++)
+		    cell += *(*(matrixA + i) + n) * *(*(matrixB + j) + n);
 		
-		*(*(matrixC + row) + col) = cell;
-    }
+		*(*(answer + i) + j) = cell;
+	    }
+	}
     
     return answer;
 }
 
 
-int main(){ //int argc, const char** argv){
-	int dim = 32; // atoi(*(argv + 1));
-	printf("\n");
-	
-	while (dim <= 1024){
-		double **matrixA = createRandomMatrix(dim);
-		double **matrixB = createRandomMatrix(dim);
-		double **matrixC = createMatrixWithZeroes(dim);
-		struct timeval start_time;
-		struct timeval end_time;
-		gettimeofday(&start_time, NULL);
-		matrixC = multiplyMatrix(matrixA, matrixB, matrixC, dim);
-		gettimeofday(&end_time, NULL);
-		double seconds = (((1000.0*end_time.tv_sec) + (end_time.tv_usec/1000.0)) -
-						 ((1000.0*start_time.tv_sec) + (start_time.tv_usec/1000.0)))/1000.0;
-		
-		//printf("\n");
-		//printf("matrix a\n");
-		//printMatrix(matrixA);
-		//printf("matrix b\n");
-		//printMatrix(matrixB);
-		//printf("matrix c\n");
-		//printMatrix(matrixC);
-		
-		printf("Taken time for a %dX%d matrix: %.5f\n",dim, dim, seconds);
-		freeMatrix(&matrixA, dim);
-		freeMatrix(&matrixB, dim);
-		freeMatrix(&matrixC, dim);
-		dim *= 2;
-	}
-	
-	printf("\n");
+int main(){
+    int dim = 32;
+    int threads = 1;
+    printf("\n---------------------------------------------\n");
     
+    while (dim <= 2048){
+        double **matrixA = createRandomMatrix(dim);
+        double **matrixB = createRandomMatrix(dim);
+        double **matrixC = createMatrixWithZeroes(dim);
+        struct timeval start_time;
+        struct timeval end_time;
+        gettimeofday(&start_time, NULL);
+        matrixC = multiplyMatrix(matrixA, matrixB, matrixC, dim, threads);
+        gettimeofday(&end_time, NULL);
+        double seconds = (((1000.0*end_time.tv_sec) + (end_time.tv_usec/1000.0)) -
+                         ((1000.0*start_time.tv_sec) + (start_time.tv_usec/1000.0)))/1000.0;
+        //printf("\n");
+        //printf("matrix a\n");
+        //printMatrix(matrixA, dim);
+        //printf("matrix b\n");
+        //printMatrix(matrixB, dim);
+        //printf("matrix c\n");
+        //printMatrix(matrixC, dim);
+        
+        printf("Taken time for a matrix of %dX%d with %d threads: %.5fs\n", dim, dim, threads, seconds);
+        freeMatrix(&matrixA, dim);
+        freeMatrix(&matrixB, dim);
+        freeMatrix(&matrixC, dim);
+        
+        threads *= 2;
+        
+        if (threads > 1){
+            printf("---------------------------------------------\n\n");
+            printf("---------------------------------------------\n");
+            threads = 1;
+            dim *= 2;
+        }
+        
+    }
 	return 0;
 }
