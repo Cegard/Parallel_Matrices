0723a30d48c3e1c4eaa3dd6686bf835297444621
Eduardo Galeano <cegard689@gmail.com>
2016-11-27 21:08:02 -0500

    code refactored

--- cudaMultiply.cu
+++ cudaMultiply.cu
@@ -3,13 +3,11 @@
 #include <sys/time.h>
 #include <time.h>
 
-#define PAD 2*sizeof(double)
-
 using namespace std;
 
 
-__host__ double* createMatrix(int dim, double zero){
-    double *matrix = (double*) malloc(dim * dim * (sizeof(double)));// * PAD));
+__host__ double* createMatrix(int dim, int zero){
+    double *matrix = (double*) malloc(dim * dim * (sizeof(double)));
     
     for (int i = 0; i < dim*dim; i++)
         *(matrix + i) = (zero)? 0.0 : (10.0*rand()/(RAND_MAX+1.0));
@@ -40,33 +38,38 @@ __host__ void printMatrix(double *matrix, int dim){
 }
 
 
+__device__ double calculateCell(int i, int dim, double *matrixA, double *matrixB){
+    double cell = 0.0;
+    
+    for (int n = 0; n < dim; n++){
+        int row = dim*(i/dim) + n;
+        int col = dim*(i%dim) + n;
+        cell += *(matrixA + row) * *(matrixB + col);
+    }
+    
+    return cell;
+}
+
+
 __global__ void multiplyMatrix(double *matrixA, double *matrixB, double *matrixC,
-                               int dim, int threads, int limit){
+                               int dim, int limit){
     int i = blockDim.x * blockIdx.x + threadIdx.x;
     
     if (i < limit){
-        double cell = 0.0;
-        for (int n = 0; n < dim; n++){
-        printf("%d %d\n", i, n);
-            int row = dim*(i/dim) + n;
-            int col = dim*(i%dim) + n;
-            cell += *(matrixA + row) * *(matrixB + col);
-        }
-        
-        *(matrixC + i) = cell;
+        *(matrixC + i) = calculateCell(i, dim, matrixA, matrixB);
     }
 }
 
 
 int main(){
-    int dim =  3;
-    int threads = 2;
+    int dim =  32;
     printf("\n---------------------------------------------\n");
-    while (dim <= 3){
+    
+    while (dim <= 2048){
         int limit = dim*dim;
-        int size = dim * dim * sizeof(double);// * PAD;
-        int blocks = 3;
-        int threadsPerBlock = 3;// + (limit/blocks);
+        int size = dim * dim * sizeof(double);
+        int blocks = 128;
+        int threadsPerBlock = dim*dim/blocks;
         
         double *h_matrixA = createRandomMatrix(dim);
         double *h_matrixB = createRandomMatrix(dim);
@@ -85,33 +88,34 @@ int main(){
         struct timeval end_time;
         gettimeofday(&start_time, NULL);
         multiplyMatrix<<<blocks, threadsPerBlock>>>(d_matrixA, d_matrixB, d_matrixC,
-                                                    dim, threads, limit);
+                                                    dim, limit);
         gettimeofday(&end_time, NULL);
         double seconds = (((1000.0*end_time.tv_sec) + (end_time.tv_usec/1000.0)) -
                          ((1000.0*start_time.tv_sec) + (start_time.tv_usec/1000.0)))/1000.0;
         cudaMemcpy(h_matrixC, d_matrixC, size, cudaMemcpyDeviceToHost);
-        printf("\n");
-        printf("matrix a\n");
-        printMatrix(h_matrixA, dim);
-        printf("matrix b\n");
-        printMatrix(h_matrixB, dim);
-        printf("matrix c\n");
-        printMatrix(h_matrixC, dim);
-        printf("Taken time for a matrix of %dX%d with %d threads: %.5fs\n", dim, dim, threads, seconds);
+        
+        //printf("\n");
+        //printf("matrix a\n");
+        //printMatrix(h_matrixA, dim);
+        //printf("matrix b\n");
+        //printMatrix(h_matrixB, dim);
+        //printf("matrix c\n");
+        //printMatrix(h_matrixC, dim);
+        printf("Taken time for a matrix of %dX%d with %d blocks and %d threads per block: %.5fs\n",
+               dim, dim, blocks, threadsPerBlock, seconds);
+        
         cudaFree(d_matrixA);
         cudaFree(d_matrixB);
         cudaFree(d_matrixC);
+        
         free(h_matrixA);
         free(h_matrixB);
         free(h_matrixC);
-        threads *= 2;
         
-        if (threads > 1){
-            printf("---------------------------------------------\n\n");
-            printf("---------------------------------------------\n");
-            threads = 1;
-            dim *= 2;
-        }
+        printf("---------------------------------------------\n\n");
+        printf("---------------------------------------------\n");
+        
+        dim *= 2;
     }
     
 	return 0;
